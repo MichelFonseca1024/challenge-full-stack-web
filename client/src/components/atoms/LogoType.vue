a3b728453cba6215a18618a770bc1a14e45a2558
Michel Fonseca <michelfonseca.nic123@gmail.com>
2025-07-07 20:54:54 -0300

    feat: add Logo component

---
+++ client/src/components/atoms/LogoType.vue
@@ -0,0 +1,32 @@
+<template>
+  <img 
+    src="@/assets/images/logo.png" 
+    :alt="alt" 
+    :width="width" 
+    :height="height" 
+    class="logo" 
+    style="object-fit: contain;"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'LogoType',
+  props: {
+    width: {
+      type: String,
+      default: '150px'
+    },
+    height: {
+      type: String,
+      default: '40px'
+    },
+    alt: {
+      type: String,
+      default: 'Logo'
+    }
+  }
+})
+</script>
