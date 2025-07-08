336c3fc524357f792948149226f140c2a6b750a1
Michel Fonseca <michelfonseca.nic123@gmail.com>
2025-07-07 22:41:36 -0300

    refactor:  replace login header text with LogoType component

--- client/src/components/molecules/LoginHeader.vue
+++ client/src/components/molecules/LoginHeader.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="text-center mb-6">
-    <h1 class="font-weight-bold mb-1 text-primary" style="font-size: 2.2rem">Edtech</h1>
+    <LogoType />
   </div>
-  <h2 class="text-h5 text-center mb-8 text-dark">Fazer Login</h2>
+  <h2 class="text-h5 text-center mb-8 text-secondary">Fazer Login</h2>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { LogoType } from '@/components/atoms'
 
 export default defineComponent({
-  name: 'LoginHeader'
+  name: 'LoginHeader',
+  components: {
+    LogoType
+  }
 })
 </script> 
